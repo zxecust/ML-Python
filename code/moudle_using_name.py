c17ee8cf68b1d811c6cdf19378320b58dc5905ea
zxecust <1850642520@qq.com>
2022-01-09 23:39:50 +0800

    update some examples about module

---
+++ code/moudle_using_name.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    print('This iprogram is being run by itself')
+else:
+    print('I am being imported from another moudle')
---
+++ code/mymodule_demo.py
@@ -0,0 +1,3 @@
+import mymodule
+mymodule.say_hi()
+print('Version',mymodule.__version__)
